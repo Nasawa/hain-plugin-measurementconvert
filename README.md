70156699ef62544e61588d17e63a709ba1634db9
Christopher Johnson <keitonasawa@gmail.com>
2016-06-03 20:26:19 -0500

    Init upload
    Added initial files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hain-plugin-measurementconvert
-A measurement converter using github.com/ben-ng/convert-units
+A measurement converter using github.com/ben-ng/convert-units for Hain, an app launcher for Windows.
+
+Usage:
+
+/convert [Measurement] [Unit of existing measurement] [Unit of desired measurement]
+
+e.g.
+To convert 50 inches to centimeters:
+
+/convert 50 in cm
+
+Commands are case insensitive.
---
+++ index.js
@@ -0,0 +1,90 @@
+/*jshint esversion: 6 */
+
+(function(){
+	'use strict';
+
+	const ncp = require('copy-paste-win32fix');
+	const convert = require('convert-units');
+
+	module.exports = (pluginContext) => {
+
+		//This controls the number of decimal places in the output answer
+		const NUMBER_OF_DECIMAL_PLACES = 4;
+
+		const toast = pluginContext.toast;
+		const logger = pluginContext.logger;
+		const app = pluginContext.app;
+
+		function search(query, res) 
+		{
+			const query_trim = query.trim();
+			const qs = query_trim.split(" ");
+
+			if (query_trim.length === 0) {
+	      		return res.add({
+	      			id: 'invalid',
+	      			payload: 'no data',
+	      			title: 'Please enter the measurement you\'d like to convert',
+	      			desc: 'e.g. 70 m ft'
+	      		});
+	    	}
+
+	    	const is_valid_amt = !(isNaN(qs[0]));
+	    	
+	    	if(!is_valid_amt){
+	    		return res.add({
+	    			id: 'invalid',
+	    			payload: 'bad number',
+	    			title: query_trim,
+	    			desc: 'Cannot convert ' + qs[0]
+	    		});
+	    	}
+
+	    	if (qs.length < 2){
+	    		return res.add({
+	    			id: 'invalid',
+	    			payload: 'too few arguments',
+	    			title: query_trim,
+	    			desc: 'Converting...'
+	    		});
+	    	}
+
+	    	if (qs.length > 3 ){
+	    		return res.add({
+	    			id: 'invalid',
+	    			payload: 'too many arguments',
+	    			title: query_trim,
+	    			desc: 'Too many arguments!'
+	    		});
+	    	}
+			
+			var result;
+			
+			if(qs.length == 2)
+				result = convert(qs[0]).from(qs[1]).toBest();
+			else
+				result = convert(qs[0]).from(qs[1]).to(qs[2]);
+
+			
+			return res.add({
+				id: 'done',
+				payload: result,
+				title: query_trim,
+				desc: `Press Enter to copy ${result} to clipboard`
+			});
+		}
+
+		function execute(id, payload){
+			if (id != 'done'){
+				return;
+			}
+
+			ncp.copy(payload, () => {
+				toast.enqueue(`Successfully copied ${payload} to the clipboard!`);
+				app.setQuery("");
+			});
+		}
+
+		return { search, execute };
+	};
+})();
